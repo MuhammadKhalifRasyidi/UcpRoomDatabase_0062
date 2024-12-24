52cbcc31593992a6434f66573e653215b7819533
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-12-24 09:35:40 +0700

    Menambahkan DAO untuk Suplier dengan fungsi insert, getAllSuplier, dan getSuplier untuk mengakses data suplier dari database.

--- app/src/main/java/com/example/ucp2/data/dao/SuplierDao.kt
+++ app/src/main/java/com/example/ucp2/data/dao/SuplierDao.kt
@@ -1,17 +1,19 @@
 package com.example.ucp2.data.dao
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import com.example.ucp2.data.entity.Suplier
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface SuplierDao {
     @Insert
     suspend fun insertSuplier(suplier: Suplier)
 
-    @Query("SELECT * FROM suplier ORDER BY nama ASC")
+    @Query("SELECT * FROM suplier ORDER BY namaS ASC")
     fun getAllSuplier(): Flow<List<Suplier>>
 
-    @Query("SELECT * FROM suplier WHERE id = :id")
-    fun getSuplier(id: String): Flow<Suplier>
+    @Query("SELECT * FROM suplier WHERE ids = :ids")
+    fun getSuplier(ids: String): Flow<Suplier>
 }
